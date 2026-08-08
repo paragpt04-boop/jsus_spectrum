c36929ac8a3dc0055c12e266cb49690114009022
paragpt04-boop <paragpt04@gmail.com>
2026-08-08 02:20:48 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -80,10 +80,10 @@ class _HomePageState extends State<HomePage>
   }
 
   Future<void> _loadAssets() async {
-    // fondo = cósmica (circle.png) ; centro = logo JSUS+ (bg.png)
-    final bg = await _loadImage('assets/circle.png');
-    final circle = await _loadImage('assets/bg.png');
-    final pal = await extractPalette('assets/bg.png');
+    // fondo = tu logo JSUS+ (bg.png -> se ve TU logo) ; centro = cósmica (circle.png)
+    final bg = await _loadImage('assets/bg.png');
+    final circle = await _loadImage('assets/circle.png');
+    final pal = await extractPalette('assets/circle.png');
     if (!mounted) return;
     setState(() {
       _bg = bg;
